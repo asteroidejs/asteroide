c16d5465c9ad0425a2955b679957ac8e3ac0625c
Lucas Larangeira <lukearch@proton.me>
2024-07-06 16:53:19 -0300

    feat(@asteroidejs/common): added HTTP_METHODS constant

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,5 +2,6 @@ export * from './enums';
 export * from './errors';
 export * from './interfaces';
 export * from './types';
+export * from './utils';
 
 export * from './directory-scanner';
---
+++ packages/common/src/utils/available-http-methods.ts
@@ -0,0 +1,3 @@
+import { HttpMethods } from '../enums';
+
+export const HTTP_METHODS = Object.values(HttpMethods);
